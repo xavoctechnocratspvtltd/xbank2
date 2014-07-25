3e010d301022647839de2da0dac9880916cf5a38
Gowrav Vishwakarma <gowravvishwakarma@gmail.com>
2014-07-25 17:48:36 +0530

    WORKING on LoanEMI Due list .. uncomplete

--- page/reports/loan/emiduelist.php
+++ page/reports/loan/emiduelist.php
@@ -22,22 +22,64 @@ class page_reports_loan_emiduelist extends Page {
 
 		$grid=$this->add('Grid'); 
 
-		$preminum_model=$this->add('Model_Premium');
+		$account_model=$this->add('Model_Active_Account_Loan');
+		$member_join=$account_model->join('members','member_id');
+		$member_join->addField('member_name','name');
+		$member_join->addField('FatherName');
+		$member_join->addField('PhoneNos');
+		$member_join->addField('PermanentAddress');
 
+		$account_model->addCondition('DefaultAC',false);
+		$account_model->addCondition('MaturedStatus',false);
+
+		$account_model->addExpression('paid_premium_count')->set(function($m,$q){
+			return $m->refSQL('Premium')
+						->addCondition('PaidOn','<>',null)
+						->addCondition('DueDate','>',$_GET['from_date']?:'1970-01-01')
+						->addCondition('DueDate','<=',$_GET['to_date']?$m->api->nextDate($_GET['to_date']):$m->api->nextDate($m->api->today))
+						->count();
+		})->sortable(true);
+
+		$account_model->addExpression('due_premium_count')->set(function($m,$q){
+			return $m->refSQL('Premium')
+						->addCondition('PaidOn',null)
+						->addCondition('DueDate','>',$_GET['from_date']?:'1970-01-01')
+						->addCondition('DueDate','<=',$_GET['to_date']?$m->api->nextDate($_GET['to_date']):$m->api->nextDate($m->api->today))
+						->count();
+		});
+
+		$account_model->addExpression('last_premium')->set(function($m,$q){
+			return $m->RefSQL('Premium')->setOrder('id','desc')->setLimit(1)->fieldQuery('DueDate');
+		});
+		
+
+		$account_model->addCondition('last_premium','>=',$this->api->today);
+		
 		if($_GET['filter']){
 
-			//TODO
+			if($_GET['dealer'])
+				$account_model->addCondition('dealer_id',$_GET['dealer']);
+
+			switch ($_GET['report_type']) {
+				case 'duelist':
+					$account_model->addCondition('due_premium_count','>',0);
+					$account_model->addCondition('due_premium_count','<=',2);
+					break;
+				
+				default:
+					# code...
+					break;
+			}
 
 		}
 
-		$grid->setModel($preminum_model);
-
+		$grid->setModel($account_model->debug(),array('AccountNumber','created_at','scheme','member_name','FatherName','PhoneNos','PermanentAddress','paid_premium_count','due_premium_count','last_premium'));
 		$grid->addPaginator(50);
 
 
 		if($form->isSubmitted()){
 
-			$send = array('dealer'=>$form['dealer'],'from_date'=>$form['from_date']?:0,'to_date'=>$form['to_date']?:0,'loan_type'=>$form['loan'],'filter'=>1);
+			$send = array('dealer'=>$form['dealer'],'from_date'=>$form['from_date']?:0,'to_date'=>$form['to_date']?:0,'report_type'=>$form['report_type'], 'loan_type'=>$form['loan'],'filter'=>1);
 			foreach ($document as $junk) {
 				if($form['doc_'.$document->id])
 					$send['doc_'.$document->id] = $form['doc_'.$document->id];

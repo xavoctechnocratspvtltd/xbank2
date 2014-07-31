79f59e2ae70e51d67ed5324f1b1d47dae402cb31
Gowrav Vishwakarma <gowravvishwakarma@gmail.com>
2014-07-31 14:37:55 +0530

    TDS Report done

--- page/reports/agent/tds.php
+++ page/reports/agent/tds.php
@@ -13,10 +13,37 @@ class page_reports_agent_tds extends Page {
 		$form->addField('DatePicker','to_date');
 		$form->addField('DropDown','account_type')->setValueList(array('%'=>'All','DDS'=>'DDS','Recurring'=>'Recurring','FD'=>'FD','MIS'=>'MIS'));
 
+		$form->addSubmit('Go');
+
 		$grid = $this->add('Grid');
 
 		$model = $this->add('Model_Agent');
 		$model->addExpression('total_commission')->set(function($m,$q){
+			
+			$tr_m = $m->add('Model_TransactionRow',array('table_alias'=>'tc'));
+			$tr = $tr_m->join('transactions','transaction_id');
+
+			$releted_account = $tr->join('accounts','reference_account_id');
+			$releted_account->addField('agent_id');
+			$releted_account->addField('account_type');
+
+			$tr_m->addCondition('agent_id',$q->getField('id'));
+			$debited_account = $tr_m->join('accounts','account_id');
+			$debited_account->addField('AccountNumber');
+
+			$tr_m->addCondition('AccountNumber','like','%Commission Paid On%');
+
+			if($_GET['account_type'])
+				$tr_m->addCondition('account_type','like',$_GET['account_type']);
+
+			if($_GET['from_date'])
+				$tr_m->addCondition('created_at','>=',date('Y-m-01',strtotime($_GET['from_date'])));
+
+			if($_GET['to_date'])
+				$tr_m->addCondition('created_at','<=',date('Y-m-t',strtotime($_GET['to_date'])));
+
+			return $tr_m->sum('amountDr');
+
 			/*
 				SUM(amountDr) 
 				where
@@ -26,7 +53,7 @@ class page_reports_agent_tds extends Page {
 			*/
 		});
 
-		$model->addExpression('total_tds')->set(function($m,$q){
+		// $model->addExpression('total_tds')->set(function($m,$q){
 			/*
 				SUM(amountCr) 
 				where
@@ -34,7 +61,7 @@ class page_reports_agent_tds extends Page {
 				row account number like TDS (under scheme duties and taxes ???)
 				created_at between filter values 01-m-Y of from_date --to-- t-m-y of to_date
 			*/
-		});
+		// });
 
 		$model->addExpression('net_commission')->set(function($m,$q){
 			/*
@@ -44,19 +71,51 @@ class page_reports_agent_tds extends Page {
 				transaction_row account_id is my account_id
 				created_at between filter values 01-m-Y of from_date --to-- t-m-y of to_date
 			*/
+		
+			$tr_m = $m->add('Model_TransactionRow',array('table_alias'=>'tc'));
+			$tr = $tr_m->join('transactions','transaction_id');
+
+			$releted_account = $tr->join('accounts','reference_account_id');
+			$releted_account->addField('agent_id');
+			$releted_account->addField('account_type');
+
+			$tr_m->addCondition('agent_id',$q->getField('id'));
+			$tr_m->addCondition('account_id',$q->getField('account_id'));
+
+			if($_GET['account_type'])
+				$tr_m->addCondition('account_type','like',$_GET['account_type']);
+
+			if($_GET['from_date'])
+				$tr_m->addCondition('created_at','>=',date('Y-m-01',strtotime($_GET['from_date'])));
+
+			if($_GET['to_date'])
+				$tr_m->addCondition('created_at','<=',date('Y-m-t',strtotime($m->api->nextDate($_GET['to_date']))));
+
+			return $tr_m->sum('amountCr');
 		});
 
 
 		if($_GET['filter']){
+			$this->api->stickyGET("filter");
+			$this->api->stickyGET("from_date");
+			$this->api->stickyGET("to_date");
+			$this->api->stickyGET("account_type");
+
 
 		}
+
+		$model->_dsql()->having('total_commission','>',0);
+		$grid->setModel($model);
+
+		$grid->add('View',null,'grid_buttons')->set('From ' . date('01-m-Y',strtotime($_GET['from_date'])). ' to ' . date('t-m-Y',strtotime($_GET['to_date'])) );
+		$grid->addPaginator(10);
 
 		if($form->isSubmitted()){
 			$grid->js()->reload(array(
 					'filter'=>1,
-					'from_date'=>$_GET['from_date']?:0,
-					'to_date'=>$_GET['to_date']?:0,
-					'account_type'=>$_GET['account_type']
+					'from_date'=>$form['from_date']?:0,
+					'to_date'=>$form['to_date']?:0,
+					'account_type'=>$form['account_type']
 				))->execute();
 		}
 

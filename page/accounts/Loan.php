a958c3c6278eeed1789b411b0df50f6ada7293a2
Gowrav Vishwakarma <gowravvishwakarma@gmail.com>
2014-08-05 14:38:47 +0530

    TRansaction statements in hook .. avoid this

--- page/accounts/Loan.php
+++ page/accounts/Loan.php
@@ -27,11 +27,11 @@ class page_accounts_Loan extends Page {
 
 			$loan_account_model = $crud->add('Model_Account_Loan');
 			try {
-				$this->api->db->beginTransaction();
+				$crud->api->db->beginTransaction();
 			    $loan_account_model->createNewPendingAccount($form['member_id'],$form['scheme_id'],$crud->api->current_branch, $form['AccountNumber'],$form->getAllFields(),$form);
-			    $this->api->db->commit();
+			    $crud->api->db->commit();
 			} catch (Exception $e) {
-			   	$this->api->db->rollBack();
+			   	$crud->api->db->rollBack();
 			   	throw $e;
 			}
 			return true;
@@ -145,11 +145,11 @@ class page_accounts_Loan extends Page {
 
 			$loan_account_model = $crud->add('Model_Account_Loan');
 			try {
-				$this->api->db->beginTransaction();
+				$crud->api->db->beginTransaction();
 			    $loan_account_model->createNewPendingAccount($form['member_id'],$form['scheme_id'],$crud->api->current_branch, $form['AccountNumber'],$form->getAllFields(),$form);
-			    $this->api->db->commit();
+			    $crud->api->db->commit();
 			} catch (Exception $e) {
-			   	$this->api->db->rollBack();
+			   	$crud->api->db->rollBack();
 			   	throw $e;
 			}
 			return true;

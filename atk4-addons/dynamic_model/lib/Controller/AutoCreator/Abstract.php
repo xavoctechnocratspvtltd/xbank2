ff2ab0b38a0f7896f88cdbd2f21781ec494364ac
Gowrav Vishwakarma <gowravvishwakarma@gmail.com>
2015-03-24 12:48:41 +0530

    Dynamic Controller ... config based and INNODB engine as default

--- atk4-addons/dynamic_model/lib/Controller/AutoCreator/Abstract.php
+++ atk4-addons/dynamic_model/lib/Controller/AutoCreator/Abstract.php
@@ -59,6 +59,10 @@ abstract class Controller_AutoCreator_Abstract extends \AbstractController
     function init() {
         parent::init();
 
+        if(!$this->api->getConfig('autocreator',true)){
+            return;
+        } 
+
         // check owner object
         $model = $this->owner;
         if (! $model instanceof \SQL_Model) {

eb522a964cc023715abe045fabd93fe94e38b4e2
sinharakesh <rksinha.btech@gmail.com>
2015-04-14 18:53:34 +0530

    usedsubmit remved from item ledger

--- page/stock/ledger/item.php
+++ page/stock/ledger/item.php
@@ -12,7 +12,7 @@ class page_stock_ledger_item extends Page {
 		// $form->addField('CheckBox','include_dead');
 		$form->addSubmit('GET');
 		$transaction=$this->add('Model_Stock_Transaction');
-		$transaction->addCondition('transaction_type','<>',array('DeadSubmit','DeadSold','Openning'));
+		$transaction->addCondition('transaction_type','<>',array('DeadSubmit','DeadSold','Openning','UsedSubmit'));
 		
 		$grid=$this->add('Grid_AccountsBase');
 

8b608503dd39a079ed51da9e0eb72d6effed6851
sinharakesh <rksinha.btech@gmail.com>
2015-06-18 12:42:14 +0530

    report value satted

--- page/reports/general/memberdepositeandloan.php
+++ page/reports/general/memberdepositeandloan.php
@@ -5,48 +5,51 @@ class page_reports_general_memberdepositeandloan extends Page {
 	function page_index(){
 		// parent::init();
 
-		$till_date="";
-		
-		if($_GET['to_date']){
-			$till_date=$_GET['to_date'];
-		}
+		$as_on_date = $this->api->today;
+		if($_GET['as_on_date'])
+			$as_on_date = $_GET['as_on_date'];
 
 		$form=$this->add('Form');
 		$form->addField('DatePicker','as_on_date');
 		$account_type=$form->addField('DropDown','status')->setValueList(array('active'=>'Active','inactive'=>'Inactive'));
 		$form->addSubmit('GET List');
 
-		$grid=$this->add('Grid');
-		$grid->add('H3',null,'grid_buttons')->set('Member Deposite and Loan Report As On '. date('d-M-Y',strtotime($till_date))); 
-		$data = array(
- 					array( 's_no' => '1', 
- 							'member_name'=>'12-09-100',
- 							'father_husband_name'=>'2000', 
- 							'sm_no'=>'',
- 							'share_account_amount'=>'any',
- 							'add_fees'=>'any',
- 							'deposite_amount'=>'1000',
- 							'loan_amount'=>'1000',
- 							'purpose_for_loan'=>'1000',
- 						)
-				);
-
-		// $account_model->add('Controller_Acl');
-		$grid->addColumn('s_no');
-		$grid->addColumn('member_name');
-		$grid->addColumn('father_husband_name');
-		$grid->addColumn('sm_no');
-		$grid->addColumn('share_account_amount');
-		$grid->addColumn('add_fees');
-		$grid->addColumn('deposite_amount');
-		$grid->addColumn('loan_amount');
-		$grid->addColumn('purpose_for_loan');
-
-		$grid->setSource($data);
+		$grid=$this->add('Grid_Report_MemberDepositeAndLoan',array('as_on_date'=>$as_on_date));
 		
+		$grid->add('H3',null,'grid_buttons')->set('Member Deposite and Loan Report As On '. date('d-M-Y',strtotime($as_on_date)));
+
+		$member_model = $this->add('Model_Member');
+		$member_model->addExpression('sm_no')->set(function($m,$q){
+			return $m->refSQL('Account')->addCondition('SchemeType','Default')->addCondition('scheme_name','Share Capital')->fieldQuery('AccountNumber');
+		});
+
+		$member_model->addExpression('share_account_amount')->set(function($m,$q){
+			return $m->refSQL('Account')->addCondition('SchemeType','Default')->addCondition('scheme_name','Share Capital')->fieldQuery('Amount');
+		});
+
+		$member_model->addExpression('add_fees')->set(function($m,$q)use($member_model){
+			$transaction_type_model = $this->add('Model_TransactionType');
+			$transaction_type_model->addCondition('name','NewMemberRegistrationAmount');
+			return $m->add('Model_Transaction')->addCondition('transaction_type_id',$transaction_type_model->fieldQuery('id'))->addCondition('reference_id',$member_model->getElement('id'))->sum('cr_sum');
+		});
+
 		if($form->isSubmitted()){
-			$grid->js()->reload(array('dealer'=>$form['dealer'],'agent'=>$form['agent'],'to_date'=>$form['to_date']?:0,'from_date'=>$form['from_date']?:0,'filter'=>1))->execute();
-		}	
+			$grid->js()->reload(array('as_on_date'=>$form['as_on_date']?:0,'filter'=>1))->execute();
+		}
+
+		if($_GET['filter']){
+			if($_GET['as_on_date'])
+				$member_model->addCondition('created_at','<=',$as_on_date);
+		}
+
+		$grid->setModel($member_model,
+								array('member_name',
+										'FatherName',
+										'sm_no',
+										'share_account_amount',
+										'add_fees',
+									)
+						);
 
 	}
 

17e7a27c32bc39e527ed18ad83a6020bcfb80a98
sinharakesh <rksinha.btech@gmail.com>
2015-06-18 15:13:13 +0530

    Closing Balance of Account report corrected

---
+++ lib/Grid/Report/ClosingBalanceOfAccount.php
@@ -0,0 +1,58 @@
+<?php
+
+class Grid_Report_ClosingBalanceOfAccount extends Grid_AccountsBase{
+	public $as_on_date;
+
+	function setModel($model,$fields=null){
+		parent::setModel($model,$fields);
+
+		//Code
+		$this->addColumn('closing_balance_of_account');
+		$this->addFormatter('closing_balance_of_account','closing_balance_of_account');
+		
+		$this->addColumn('sm_account_no');
+		$this->addFormatter('sm_account_no','sm_account_no');
+
+		$this->addFormatter('member','Wrap');
+		$this->addFormatter('PermanentAddress','Wrap');
+		$this->addSno();
+		$this->addPaginator(5);
+
+	}
+
+	function format_closing_balance_of_account($field){
+		//rd,dds,fd,mis Account
+		$account_model	= $this->add('Model_Account');
+		$account_model->addCondition('member_id',$this->model->id);
+		$account_model->addCondition('SchemeType',array('DDS',ACCOUNT_TYPE_FIXED,'Recurring'));
+		$cr = 0;
+		$dr = 0;
+		
+		foreach ($account_model as $account) {
+			$array = $account->getOpeningBalance($this->api->nextDate($this->as_on_date));
+			$cr += $array['CR'];
+			$dr += $array['DR'];
+		}	
+
+		$amount = $cr-$dr;
+		$balance = $amount.' CR';
+		if($amount < 0)
+			$balance = abs($amount).' DR';
+
+		$this->current_row[$field] = $balance;
+	}
+
+	function format_sm_account_no($field){
+		if(!$this->model['member_id'])
+			$this->current_row[$field] = "Member Not Found";
+			
+		$member_model = $this->add('Model_Member')->load($this->model['member_id']);
+		$number = $member_model->ref('Account')->addCondition('SchemeType','Default')->addCondition('scheme_name','Share Capital')->fieldQuery('AccountNumber');
+
+		$this->current_row[$field] = $number; 
+	}
+
+	// function formatRow(){
+	// 	parent::formatRow();
+	// }
+}
--- page/reports/general/closingbalanceofaccount.php
+++ page/reports/general/closingbalanceofaccount.php
@@ -7,43 +7,35 @@ class page_reports_general_closingbalanceofaccount extends Page {
 
 		$till_date="";
 		
-		if($_GET['to_date']){
-			$till_date=$_GET['to_date'];
+		if($_GET['as_on_date']){
+			$till_date=$this->api->nextDate($_GET['as_on_date']);
 		}
 
 		$form=$this->add('Form');
 		$form->addField('DatePicker','as_on_date');
-		$account_type=$form->addField('autocomplete/Basic','account_type');
+		$account_type=$form->addField('DropDown','account_type');
+		$account_type->setValueList(explode(',', ACCOUNT_TYPES))->setEmptyText('Select Account type');
+
 		$form->addSubmit('GET List');
 
-		$grid=$this->add('Grid');
-		$grid->add('H3',null,'grid_buttons')->set('Account Close Report As On '. date('d-M-Y',strtotime($till_date))); 
-		$data = array(
- 					array( 's_no' => '1', 
- 							'account_no' => 'Smith', 
- 							'member_name'=>'12-09-100',
- 							'father_husband_name'=>'2000', 
- 							'address'=>'any',
- 							'phone_no'=>'any',
- 							'closing_balance_of_account'=>'1000',
- 							'sm_account_no'=>'' 
- 						)
-				);
+		$grid=$this->add('Grid_Report_ClosingBalanceOfAccount',array('as_on_date'=>$till_date));
+		$grid->add('H3',null,'grid_buttons')->set('Account Close Report As On '. date('d-M-Y',strtotime($till_date)));
+		
+		$account_model = $this->add('Model_Account');
+		$member_join = $account_model->leftJoin('members','member_id');
+		$member_join->addField('FatherName')->caption('Father/Husband Name');
+		$member_join->addField('PermanentAddress');
+		$member_join->addField('PhoneNos');
 
-		// $account_model->add('Controller_Acl');
-		$grid->addColumn('s_no');
-		$grid->addColumn('account_no');
-		$grid->addColumn('member_name');
-		$grid->addColumn('father_husband_name');
-		$grid->addColumn('address');
-		$grid->addColumn('phone_no');
-		$grid->addColumn('closing_balance_of_account');
-		$grid->addColumn('sm_account_no');
-
-		$grid->setSource($data);
+		if($_GET['filter']){
+			if($_GET['account_type'])
+				$account_model->addCondition('scheme_type',$_GET['account_type']);
+		}
+	
+		$grid->setModel($account_model,array('AccountNumber','member','FatherName','PermanentAddress','PhoneNos'));
 		
 		if($form->isSubmitted()){
-			$grid->js()->reload(array('dealer'=>$form['dealer'],'agent'=>$form['agent'],'to_date'=>$form['to_date']?:0,'from_date'=>$form['from_date']?:0,'filter'=>1))->execute();
+			$grid->js()->reload(array('as_on_date'=>$form['as_on_date']?:0,'account_type'=>$form['account_type'],'filter'=>1))->execute();
 		}	
 
 	}

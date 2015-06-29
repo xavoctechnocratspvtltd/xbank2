a26c83f40271527516ad12d3e8c4591c3aa646b2
sinharakesh <rksinha.btech@gmail.com>
2015-06-29 13:51:27 +0530

    Grid TDS and Report add

--- page/reports/deposit/tdsquaterly.php
+++ page/reports/deposit/tdsquaterly.php
@@ -10,11 +10,11 @@ class page_reports_deposit_tdsquaterly extends Page {
 
 		$form->addSubmit('GET List');
 
-		$grid = $this->add('Grid_AccountsBase');
-		$grid->add('H3',null,'grid_buttons')->set('TDS Report As On ');
+		$grid = $this->add('Grid_Report_TdsQuaterly');
 
 		$model = $this->add('Model_Transaction');
 		$model->addCondition('transaction_type',array(TRA_ACCOUNT_OPEN_AGENT_COMMISSION,TRA_PREMIUM_AGENT_COMMISSION_DEPOSIT,TRA_PREMIUM_AGENT_COLLECTION_CHARGE_DEPOSIT));
+		$model->addCondition('branch_id',$this->api->current_branch->id);
 
 		$reference_account_j = $model->join('accounts','reference_id');
 		$reference_account_j->addField('agent_id');
@@ -26,53 +26,46 @@ class page_reports_deposit_tdsquaterly extends Page {
 		$model->addExpression('tds_amt')->set($model->refSQL('TransactionRow')->addCondition('account','like','%TDS%')->sum('amountCr'));
 		$model->addExpression('total_comm')->set($model->refSQL('TransactionRow')->addCondition('account','not like','%TDS%')->sum('amountCr'));
 		$model->addExpression('tds_per')->set('10');
+		$model->addExpression('month')->set('DATE_FORMAT('.$model->dsql()->getField('created_at').',"%M")');
 
 		$model->getElement('reference_id')->caption('Account');
 		$model->getElement('dr_sum')->caption('Net Amount');
 		$model->getElement('agent_id')->caption('Name and Address');
 
-
-		// if($_GET['filter']){
-		// 	$date = $this->api->today;
-			// if($_GET['qtr']){
-			// 	$this->api->stickyGET('qtr');
-			// 	$year = date('Y',strtotime($date));
-			// 	$date = $year.'-'.$_GET['qtr'].'-'.'01';
-			// }
+		if($_GET['filter']){
+			$this->api->stickyGET('filter');
+			$date = $this->api->today;
+			if($_GET['qtr']){
+				$this->api->stickyGET('qtr');
+				$year = date('Y',strtotime($date));
+				$date = $year.'-'.$_GET['qtr'].'-'.'01';
+			}
 
 			
-			// $quarter_date = $this->api->getFinancialQuarter($date);
-			// $to_date = $quarter_date['start_date'];
-			// $from_date = $quarter_date['end_date'];
+			$quarter_date = $this->api->getFinancialQuarter($date);
+			$from_date = $quarter_date['start_date'];
+			$to_date = $quarter_date['end_date'];
 
-			// // throw new \Exception($date.'::'.$to_date."::".$from_date);
-			// $model->addCondition('created_at','>=',date('Y-m-01',strtotime($from_date)));
-			// $model->addCondition('created_at','<=',date('Y-m-t',strtotime($to_date)));
+			$grid->add('H3',null,'grid_buttons')->set('TDS Report From: '.$from_date.' To Date'.$to_date);
+			// throw new \Exception($date.'::'.$to_date."::".$from_date);
+			$model->addCondition('created_at','>=',date('Y-m-01',strtotime($from_date)));
+			$model->addCondition('created_at','<=',date('Y-m-d',strtotime($to_date)));
 
-		// }else
-			// $model->addCondition('id',-1);
+		}else
+			$model->addCondition('id',-1);
 		
-		$model->setLimit(10);
+		// $model->setLimit(10);
+		$model->setOrder('created_at');
+		$model->setOrder('agent_id');
 		$model->_dsql()->group('agent_id');
-		$grid->setModel($model,array('agent_id','agent','PanNo','tds_per','total_comm','tds_amt','dr_sum'));
+		$model->_dsql()->group('month');
+
+		$grid->setModel($model,array('agent_id','agent','PanNo','tds_per','total_comm','tds_amt','dr_sum','created_at','month'));
 		
-		$grid->addColumn('month');
-		$grid->addColumn('date_of_tds');
+		// $grid->addColumn('month');
+		// $grid->addColumn('date_of_tds');
 		$grid->addColumn('ch_no');
-
-		$grid->addMethod('format_agent_id',function($g,$f){
-				if($g->model['agent_id']){
-					$agent_m = $g->add('Model_Agent')->tryLoad($g->model['agent_id']);				
-					$g->current_row[$f] = $agent_m['name'];
-				}
-				
-			});
-		$grid->addFormatter('agent_id','agent_id,Wrap');
-
-		$grid->add('View',null,'grid_buttons')->set('From ' . date('01-m-Y',strtotime($_GET['from_date'])). ' to ' . date('t-m-Y',strtotime($_GET['to_date'])) );
-		$grid->addPaginator(50);
-		$grid->addSno();
-
+		
 		if($form->isSubmitted()){
 			$grid->js()->reload(array(
 					'filter'=>1,

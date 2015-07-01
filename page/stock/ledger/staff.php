203b4b43395cc5e3421da25d4a00f6bfe657d498
sinharakesh <rksinha.btech@gmail.com>
2015-07-01 15:10:26 +0530

    staff ledger staffcondition working now

--- page/stock/ledger/staff.php
+++ page/stock/ledger/staff.php
@@ -22,6 +22,8 @@ class page_stock_ledger_staff extends Page {
 
 		$str = "Staff Leadger";
 		if($_GET['filter']){
+			
+			$this->api->stickyGET('staff');
 			$staff_model = $this->add('Model_Stock_Staff')->load($_GET['staff']);
 			$item_model = $this->add('Model_Stock_Item');
 			$item_model->addCondition('id',$_GET['item']);
@@ -30,18 +32,20 @@ class page_stock_ledger_staff extends Page {
 			if($item_model->loaded())
 				$item_name = $item_model['name'];
 			$str = "Staff ( ".$staff_model['name']." ) Ledger on Item ( ".$item_name." ) From Date: ".$_GET['from_date']." To date: ".$_GET['to_date'];
+
+			$scl_tab->add('View_Info')->set($str)->setStyle(array('padding'=>'2px','margin'=>'5px 0 5px 0'));
+			$view_consume=$scl_tab->add('View_StockMember_Consume',array('item'=>$_GET['item'],'member'=>$_GET['staff'],'from_date'=>$_GET['from_date'],'to_date'=>$form['to_date'],'filter'=>$_GET['filter'],'type'=>'Staff'));
+			
+			$sil_tab->add('View_Info')->set($str)->setStyle(array('padding'=>'2px','margin'=>'5px 0 5px 0'));
+			$view_issue=$sil_tab->add('View_StockMember_Issue',array('item'=>$_GET['item'],'member'=>$_GET['staff'],'from_date'=>$_GET['from_date'],'to_date'=>$_GET['to_date'],'filter'=>$_GET['filter'],'type'=>'Staff'));
+
+			$sfl_tab->add('View_Info')->set($str)->setStyle(array('padding'=>'2px','margin'=>'5px 0 5px 0'));
+			$view_fixed=$sfl_tab->add('View_StockMember_FixedAssets',array('item'=>$_GET['item'],'member'=>$_GET['staff'],'from_date'=>$_GET['from_date'],'to_date'=>$_GET['to_date'],'filter'=>$_GET['filter'],'type'=>'Staff'));
 		}
 
-		$scl_tab->add('View_Info')->set($str)->setStyle(array('padding'=>'2px','margin'=>'5px 0 5px 0'));
-		$view_consume=$scl_tab->add('View_StockMember_Consume',array('item'=>$_GET['item'],'member'=>$_GET['staff'],'from_date'=>$_GET['from_date'],'to_date'=>$form['to_date'],'filter'=>$_GET['filter'],'type'=>'Staff'));
-		
-		$sil_tab->add('View_Info')->set($str)->setStyle(array('padding'=>'2px','margin'=>'5px 0 5px 0'));
-		$view_issue=$sil_tab->add('View_StockMember_Issue',array('item'=>$_GET['item'],'member'=>$_GET['staff'],'from_date'=>$_GET['from_date'],'to_date'=>$_GET['to_date'],'filter'=>$_GET['filter'],'type'=>'Staff'));
+		// throw new \Exception("Filter".$_GET['staff'], 1);
 
-		$sfl_tab->add('View_Info')->set($str)->setStyle(array('padding'=>'2px','margin'=>'5px 0 5px 0'));
-		$view_fixed=$sfl_tab->add('View_StockMember_FixedAssets',array('item'=>$_GET['item'],'member'=>$_GET['staff'],'from_date'=>$_GET['from_date'],'to_date'=>$_GET['to_date'],'filter'=>$_GET['filter'],'type'=>'Staff'));
-
-		if($form->isSubmitted()){
+		if($form->isSubmitted()){			
 			$v->js()->reload(array('item'=>$form['item'],'staff'=>$form['staff'],'from_date'=>$form['from_date']?:0,'to_date'=>$form['to_date']?:0,'filter'=>1))->execute();
 		}
 

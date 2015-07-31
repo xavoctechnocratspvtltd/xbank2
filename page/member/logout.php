8064ec40c1d353fe849c672ed62f5a802c06d929
xavocvijay <guddavijay@gmail.com>
2015-07-31 15:58:34 +0530

    dsa logout & member logout session out

--- page/member/logout.php
+++ page/member/logout.php
@@ -4,6 +4,7 @@ class page_member_logout extends Page{
 	function init(){
 		parent::init();
 
+		$this->api->destroySession();
 		$this->api->auth->logout();
 		$this->api->redirect('member_dashboard');
 	}

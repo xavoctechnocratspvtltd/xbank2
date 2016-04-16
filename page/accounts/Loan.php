4004e18f2f0d6729dfe56da1d4d3d23e100b8cb6
Gowrav Vishwakarma <gowravvishwakarma@gmail.com>
2016-04-16 16:16:55 +0530

    Correct percentage for loan

--- page/accounts/Loan.php
+++ page/accounts/Loan.php
@@ -51,7 +51,7 @@ class page_accounts_Loan extends Page {
 					$form->displayError('MinorNomineeParentName','mandatory field');
 				}
 				if($loan_amount > $amount_allowed)
-					$form->displayError('Amount',"Amount is grater than 80% of  FD Amount");
+					$form->displayError('Amount',"Amount is grater than ".$account['percent_loan_on_deposit']."% of  FD Amount");
 			}
 
 			if($crud->isEditing('edit')) {

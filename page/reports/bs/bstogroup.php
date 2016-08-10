03298cce93bde0c1429b2e8de7e8738d07a51222
Gowrav Vishwakarma <gowravvishwakarma@gmail.com>
2016-08-10 18:36:50 +0530

    trying with scheme group

--- page/reports/bs/bstogroup.php
+++ page/reports/bs/bstogroup.php
@@ -10,31 +10,19 @@ class page_reports_bs_bstogroup extends Page{
 		$to_date = $this->api->stickyGET('to_date');
 		$branch_id = $this->api->stickyGET('branch_id');
 
-
-		// $st=$this->add('Model_Scheme');
-		// $st->addCondition('balance_sheet_id',$bs_id);
-
-		// foreach ($st as $s) {
-		// 	echo $s['name']. '</br>';
-		// }
-
-		// return;
-
-		// var_dump($this->api->db->dsql()->expr('select * from schemes where balance_sheet_id=2')->get());
-		// exit;
-		$op_balances_q='select a.scheme_id id, sum(IFNULL(OpeningBalanceDr,0)) DR, sum(IFNULL(OpeningBalanceCr,0)) CR
+		$op_balances_q='select a.SchemeGroup id, sum(IFNULL(OpeningBalanceDr,0)) DR, sum(IFNULL(OpeningBalanceCr,0)) CR
 						from accounts a
 						join schemes s on a.scheme_id = s.id
 						WHERE s.balance_sheet_id = '.$bs_id.'
 						';
 		if($branch_id) $op_balances_q .= ' AND a.branch_id = ' . $branch_id;
-		$op_balances_q .= ' group by a.scheme_id';
+		$op_balances_q .= ' group by a.SchemeGroup';
 
 		$op_balances = $this->api->db->dsql()->expr($op_balances_q)->get();
 
 		// var_dump($op_balances);
 
-		$prev_transactions_q  = 'select a.scheme_id id, sum(IFNULL(amountDr,0)) DR, sum(IFNULL(amountCr,0)) CR
+		$prev_transactions_q  = 'select a.SchemeGroup id, sum(IFNULL(amountDr,0)) DR, sum(IFNULL(amountCr,0)) CR
 								from transaction_row tr 
 								join accounts a on tr.account_id = a.id
 								join schemes s on a.scheme_id = s.id
@@ -42,12 +30,12 @@ class page_reports_bs_bstogroup extends Page{
 								and s.balance_sheet_id = '.$bs_id.'
 								';
 		if($branch_id) $prev_transactions_q .= ' and a.branch_id = ' . $branch_id;
-		$prev_transactions_q .=	' group by a.scheme_id';
+		$prev_transactions_q .=	' group by a.SchemeGroup';
 		$prev_transactions = $this->api->db->dsql()->expr($prev_transactions_q)->get();
 
 		// var_dump($prev_transactions);
 
-		$curr_trans_q='select a.scheme_id id, sum(IFNULL(amountDr,0)) DR, sum(IFNULL(amountCr,0)) CR
+		$curr_trans_q='select a.SchemeGroup id, sum(IFNULL(amountDr,0)) DR, sum(IFNULL(amountCr,0)) CR
 					from transaction_row tr 
 					join accounts a on tr.account_id = a.id
 					join schemes s on a.scheme_id = s.id
@@ -55,7 +43,7 @@ class page_reports_bs_bstogroup extends Page{
 					and s.balance_sheet_id = '.$bs_id.'
 					';
 		if($branch_id) $curr_trans_q .= ' and a.branch_id = ' . $branch_id;
-		$curr_trans_q.=' group by a.scheme_id';
+		$curr_trans_q.=' group by a.SchemeGroup';
 		
 		$curr_trans = $this->api->db->dsql()->expr($curr_trans_q)->get();
 

c8e6abc64c2666afbd4961ee00c8fc545e576728
Gowrav Vishwakarma <gowravvishwakarma@gmail.com>
2016-08-10 12:28:41 +0530

    Bugs fixed

--- lib/Model/BS/TransactionRow.php
+++ lib/Model/BS/TransactionRow.php
@@ -1,7 +1,11 @@
 <?php
 
-class Model_BS_TransactionRow extends MOdel_TransactionRow {
+class Model_BS_TransactionRow extends Model_TransactionRow {
 	
+	public $from_date=null;
+	public $to_date=null;
+	public $branch_id = null;
+
 	function init(){
 		parent::init();
 
@@ -9,6 +13,12 @@ class Model_BS_TransactionRow extends MOdel_TransactionRow {
 			$acc= $this->add('Model_BS_Ledger');
 			$acc->addCondition('id',$q->getField('account_id'));
 			return $acc->fieldQuery('balance_sheet_id');
+		});
+
+		$this->addExpression('scheme_id')->set(function($m,$q){
+			$acc= $this->add('Model_BS_Ledger');
+			$acc->addCondition('id',$q->getField('account_id'));
+			return $acc->fieldQuery('scheme_id');
 		});
 	}
 }

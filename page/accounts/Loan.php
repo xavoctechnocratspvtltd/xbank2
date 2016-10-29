d07ffd27d45ef7fab5c5c906311fa89ad3da2e20
Gowrav Vishwakarma <gowravvishwakarma@gmail.com>
2016-10-29 13:09:02 +0530

    Only active memebr list

--- page/accounts/Loan.php
+++ page/accounts/Loan.php
@@ -194,7 +194,7 @@ class page_accounts_Loan extends Page {
 
 		if($crud->isEditing('add')){
 			// $crud->form->getElement('member_id')->getModel()->addOkConditions();
-
+			$crud->form->getElement('member_id')->getModel()->addCondition('is_active',true);
 			$crud->form->getElement('scheme_id')->getModel()->addCondition('ActiveStatus',true);
 
 			// $crud->form->add('Order')

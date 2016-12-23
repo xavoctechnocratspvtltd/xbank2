04c6ef7c487aa585e9c8d3edf4c59facb2ce3419
Gowrav Vishwakarma <gowravvishwakarma@gmail.com>
2016-12-23 13:26:58 +0530

    test some condition, why it was commenedt while required now

--- page/reports/loan/newNPAaccount.php
+++ page/reports/loan/newNPAaccount.php
@@ -169,7 +169,7 @@ class page_reports_loan_newNPAaccount extends Page {
 				case 'five_above':
 					$account_model->addCondition('due_premium_count','>',5);
 					$account_model->addCondition('last_premium','>=',$this->api->previousMonth($this->api->today. " -5 MONTH"));
-					// $account_model->addCondition($account_model->dsql()->expr('[0] < "[1]"',array($account_model->getElement('last_premium'),$till_date)));
+					$account_model->addCondition($account_model->dsql()->expr('[0] < "[1]"',array($account_model->getElement('last_premium'),$till_date)));
 					break;
 				
 				default:

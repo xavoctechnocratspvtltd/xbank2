5c6efbc45395abe1bf14515708dd1d4684efb29f
vijay mali <guddavijay@gmail.com>
2017-02-03 11:42:15 +0530

    sms api url password urlencode done

--- lib/Controller/Sms.php
+++ lib/Controller/Sms.php
@@ -8,9 +8,8 @@ class Controller_Sms extends AbstractController{
 	function sendMessage($no,$msg){
 		$curl=$this->add('Controller_CURL');
 		$msg=urlencode($msg);
-
-		$url="http://cloud.smsindiahub.in/vendorsms/pushsms.aspx?user=".$this->app->getConfig('user')."&password=".$this->app->getConfig('password')."&msisdn=$no&sid=".$this->app->getConfig('senderId')."&msg=$msg&fl=0&gwid=2";
-		
+		$password = urlencode($this->app->getConfig('password'));
+		$url="http://cloud.smsindiahub.in/vendorsms/pushsms.aspx?user=".$this->app->getConfig('user')."&password=".$password."&msisdn=$no&sid=".$this->app->getConfig('senderId')."&msg=$msg&fl=0&gwid=2";
 		// echo $url;
 		return $curl->get($url);
 	}

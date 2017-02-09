4b6b42f06c4d7f0ab6b693cc023caf3725b59d40
Gowrav Vishwakarma <gowravvishwakarma@gmail.com>
2017-02-09 20:23:48 +0530

    some checks

--- page/accounts/Loan.php
+++ page/accounts/Loan.php
@@ -502,6 +502,14 @@ class page_accounts_Loan extends Page {
 	function page_bike_legal(){
 		$model= $this->add('Model_Active_Account_Loan');
 		$model->getElement('AccountNumber')->readOnly(true);
+
+		$model->addHook('beforeSave',function($m){
+			if(!$m['bike_surrendered'] && $m['bike_surrendered_on'])
+				throw $m->exception('Please select this','ValidityCheck')->setField('bike_surrendered');
+			if(!$m['is_in_legal'] && $m['legal_filing_date'])
+				throw $m->exception('Please select this','ValidityCheck')->setField('is_in_legal');
+		});
+
 		$crud = $this->add('CRUD',['allow_add'=>false, 'allow_del'=>false]);
 		$crud->setModel($model,['bike_surrendered','bike_surrendered_on','is_in_legal','legal_filing_date'],['AccountNumber','bike_surrendered','bike_surrendered_on','is_in_legal','legal_filing_date']);
 		$crud->add('Controller_Acl');

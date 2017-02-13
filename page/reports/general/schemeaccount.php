f65308ef1c91a7ba86be31da16bf5fe7b1ac403d
vijay mali <guddavijay@gmail.com>
2017-02-13 17:50:52 +0530

    scheme account report data done

--- page/reports/general/schemeaccount.php
+++ page/reports/general/schemeaccount.php
@@ -7,6 +7,13 @@ class page_reports_general_schemeaccount extends Page {
 
 		$from_date = '1970-01-02';
 		$to_date = $this->api->today;
+
+		$filter = $this->api->stickyGET('filter');
+		$from_date = $this->api->stickyGET('from_date');
+		$to_date = $this->api->stickyGET('to_date');
+		$selected_scheme = $this->api->stickyGET('scheme');
+		$this->api->stickyGET('account_status');
+		$status = $this->api->stickyGET('scheme_status');
 		
 		$scheme_m = $this->add('Model_Scheme');
 
@@ -17,7 +24,8 @@ class page_reports_general_schemeaccount extends Page {
 		$form=$this->add('Form');
 		$form->addField('DatePicker','from_date');
 		$form->addField('DatePicker','to_date');
-		$form->addField('dropdown','status')->setValueList(array('all'=>'All','0'=>'InActive','1'=>'Active'));
+		$form->addField('dropdown','scheme_status')->setValueList(array('all'=>'All','0'=>'InActive','1'=>'Active'));
+		$form->addField('dropdown','account_status')->setValueList(array('all'=>'All','0'=>'InActive','1'=>'Active'));
 		$account_type=$form->addField('DropDown','account_type');
 		$array_value = $array_key = explode(',', ACCOUNT_TYPES);
 		$account_type->setValueList(array_combine($array_key, $array_value))->setEmptyText('Select Account type');
@@ -37,26 +45,32 @@ class page_reports_general_schemeaccount extends Page {
 		$scheme = $this->add('Model_Scheme');
 
 		$scheme->addExpression('account_count')->set(function($m,$q){
-			return $m->add('Model_Account')
-					->addCondition('scheme_id',$q->getField('id'))
-					->count();
+			$sum_acc = $m->add('Model_Account')
+					->addCondition('scheme_id',$q->getField('id'));
+			if($_GET['account_status'] !=='all'){
+				$sum_acc->addCondition('ActiveStatus',$_GET['status']==0?false:true);
+			}		
+					
+			return $sum_acc->count();
 		});
 		$scheme->addExpression('sum_amount')->set(function($m,$q){
-			return $m->add('Model_Account')
-					->addCondition('scheme_id',$q->getField('id'))
-					->sum('Amount');
+			$sum_acc = $m->add('Model_Account')
+					->addCondition('scheme_id',$q->getField('id'));
+			if($_GET['account_status'] !=='all'){
+				$sum_acc->addCondition('ActiveStatus',$_GET['status']==0?false:true);
+			}		
+					
+			return $sum_acc->sum('Amount');		
 		});
 		
-		$filter = $this->api->stickyGET('filter');
-		$from_date = $this->api->stickyGET('from_date');
-		$to_date = $this->api->stickyGET('to_date');
-		$selected_scheme = $this->api->stickyGET('scheme');
-		$status = $this->api->stickyGET('status');
 
 		if($_GET['filter']){
-			if($account_type){
+			if($_GET['account_type']){
 				$selected_account_type = $this->api->stickyGET('account_type');
 				$scheme->addCondition('SchemeType',$selected_account_type);
+			}
+			if($_GET['scheme_status'] !=='all'){
+				$scheme->addCondition('ActiveStatus',$_GET['scheme_status']==0?false:true);
 			}
 			if($selected_scheme){
 				$scheme->addCondition('id',$selected_scheme);
@@ -68,12 +82,9 @@ class page_reports_general_schemeaccount extends Page {
 			if($_GET['to_date']){
 				$scheme->addCondition('created_at','<',$this->api->nextDate($_GET['to_date']));
 			}
-			if($_GET['status'] !=='all'){
-				$scheme->addCondition('ActiveStatus',$_GET['status']==0?false:true);
-			}
 		}
 		// $scheme->add('Controller_Acl');
-		$grid->setModel($scheme,array('name','account_count','sum_amount'));
+		$grid->setModel($scheme,array('account_count','sum_amount'));
 		
 		if($form->isSubmitted()){
 			$grid->js()->reload(
@@ -81,7 +92,8 @@ class page_reports_general_schemeaccount extends Page {
 								'to_date'=>$form['to_date']?:0,
 								'from_date'=>$form['from_date']?:0,
 								'account_type'=>$form['account_type'],
-								'status'=>$form['status'],
+								'scheme_status'=>$form['scheme_status'],
+								'account_status'=>$form['account_status'],
 								'scheme'=>$form['scheme'],
 								'filter'=>1
 							)

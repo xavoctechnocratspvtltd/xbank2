563a19014bd585f31cf6006bfbe3ebe271ff33bf
Gowrav Vishwakarma <gowravvishwakarma@gmail.com>
2017-02-17 13:49:14 +0530

    added report for dealer wise receive in loan accounts

--- page/reports/recovery.php
+++ page/reports/recovery.php
@@ -22,6 +22,7 @@ class page_reports_recovery extends Page {
 		// $tab1=$tabs->addTabURL('reports_loan_documentrenew','CC Document Renew Report');
 		$tab1=$tabs->addTabURL('reports_loan_newNPAaccount','New NPA Account List');
 		$tab1=$tabs->addTabURL('reports_loan_dealerwise','Dealer Wise Recovery');
+		$tab1=$tabs->addTabURL('reports_loan_dealerwisereceived','Dealer Wise Received');
 			
 
 	}

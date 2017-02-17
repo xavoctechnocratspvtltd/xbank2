401e8222d28bad45078e9e74b034ab646a476b85
Gowrav Vishwakarma <gowravvishwakarma@gmail.com>
2017-02-17 11:48:06 +0530

    Removed back three columns from day and cash book

--- page/reports/cashbook.php
+++ page/reports/cashbook.php
@@ -25,11 +25,11 @@ class page_reports_cashbook extends Page {
 		$scheme_join = $account_join->join('schemes','scheme_id');
 		$scheme_join->addField('scheme_name','name');
 
-		$ref_account = $cash_transaction_model->join('accounts','reference_id');
-		$ref_account_member = $ref_account->join('members','member_id');
-		$ref_account_member->addField('member_name','name');
-		$ref_account_member->addField('PermanentAddress');
-		$ref_account_member->addField('PanNo');
+		// $ref_account = $cash_transaction_model->join('accounts','reference_id');
+		// $ref_account_member = $ref_account->join('members','member_id');
+		// $ref_account_member->addField('member_name','name');
+		// $ref_account_member->addField('PermanentAddress');
+		// $ref_account_member->addField('PanNo');
 
 		$cash_transaction_model->addCondition('scheme_name',CASH_ACCOUNT);
 		$cash_transaction_model->setOrder('voucher_no');

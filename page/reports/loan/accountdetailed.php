07b44f950be28beb29fa1030bc38ff7203bb5c71
Gowrav Vishwakarma <gowravvishwakarma@gmail.com>
2017-04-04 15:44:16 +0530

    account detials file added back from last commit

---
+++ page/reports/loan/accountdetailed.php
@@ -0,0 +1,53 @@
+<?php
+class page_reports_loan_accountdetailed extends Page {
+	public $title="Loan Account Detailed Report";
+	function init(){
+		parent::init();
+
+
+		$form=$this->add('Form');
+		$accounts_no_field=$form->addField('autocomplete/Basic','accounts_no');
+		$accounts=$this->add('Model_Account');
+		$accounts_no_field->setModel($accounts);
+
+		$form->addSubmit('GET List');
+
+
+		// $grid=$this->add('Grid'); 
+
+		// $accounts_model=$this->add('Model_Account_DDS');
+		$accounts_model=$this->add('Model_Account');
+		// $accounts_model->setOrder('id','desc');
+
+
+		if($this->api->stickyGET('accounts_no')){
+			$accounts_model->load($_GET['accounts_no']);
+		}else{
+
+		}
+
+		$account_view = $this->add('View_AccountDetail',array('account'=>$accounts_model));
+		
+		// $grid->setModel($accounts_model);
+
+		// $grid->addPaginator(50);
+
+		$js=array(
+			$this->js()->_selector('.mymenu')->parent()->parent()->toggle(),
+			$this->js()->_selector('#header')->toggle(),
+			$this->js()->_selector('#footer')->toggle(),
+			$this->js()->_selector('ul.ui-tabs-nav')->toggle(),
+			$this->js()->_selector('.atk-form')->toggle(),
+			);
+
+		$account_view->js('click',$js);
+
+
+		if($form->isSubmitted()){
+			$account_view->js()->reload(array('accounts_no'=>$form['accounts_no']))->execute();
+
+		}	
+	
+
+	}
+}

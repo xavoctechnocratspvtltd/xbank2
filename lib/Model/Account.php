f7b85a554813b63a0ed7e556be07b686c9cd49b5
Gowrav Vishwakarma <gowravvishwakarma@gmail.com>
2017-04-20 15:54:44 +0530

    by pass commission to sponsor if agent and sponsor is on same level

--- lib/Model/Account.php
+++ lib/Model/Account.php
@@ -627,6 +627,9 @@ class Model_Account extends Model_Table {
 		
 		$agent = $this->agent();
 		while($sponsor = $agent->sponsor()){
+
+			// do not give sponsor commission if agent and sponsor on same level
+			if($agent->cadre()->get('id') == $sponsor()->cadre()->get('id')) continue;
 			
 			$percentage = $sponsor->cadre()->cumulativePercantage($agent->cadre());
 			$commissionForThisAgent = $total_commission_amount * $percentage / 100;

cfc4398d448020c58f39db3cfbe2cdcca8fee81d
Gowrav Vishwakarma <gowravvishwakarma@gmail.com>
2017-09-20 17:01:42 +0530

    changed deposit sms template

--- page/transactions/deposit.php
+++ page/transactions/deposit.php
@@ -182,7 +182,7 @@ class page_transactions_deposit extends Page {
 			   	throw $e;
 			}
 			$member=$account_model->ref('member_id');
-			$msg="Dear Member, your account ".$account_model['AccountNumber']." has been credited with amount ".$form['amount'].".";
+			$msg="Dear Member, your account ".$account_model['AccountNumber']." has been credited with amount ".$form['amount']." on ". $this->app->today. " From:- Bhawani Credit Co-Operative Society Ltd. +91 8003597814";
 			
 			$mobile_no=explode(',', $member['PhoneNos']);
 			if(strlen(trim($mobile_no[0])) == 10){

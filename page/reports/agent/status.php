437ffe37ff80d20438c34d7e8b19170cac090cec
Gowrav Vishwakarma <gowravvishwakarma@gmail.com>
2018-03-07 14:27:56 +0530

    added mo in report field

--- page/reports/agent/status.php
+++ page/reports/agent/status.php
@@ -13,7 +13,12 @@ class page_reports_agent_status extends Page {
 		}
 
 		$form=$this->add('Form');
-		$form->addField('dropdown','status')->setValueList(array('Inactive'=>'Inactive','Active'=>'Active'))->setEmptyText('Please Select')->validateNotNull();
+		$mo_f = $form->addField('autocomplete/Basic','current_mo_of_agents');
+		$mo_f->setModel('Mo');
+
+		$form->addField('autocomplete/Basic','agent')->setModel('Agent');
+
+		$form->addField('dropdown','status')->setValueList(array('Inactive'=>'Inactive','Active'=>'Active'))->setEmptyText('All');
 		$form->addSubmit('GET LIST');
 
 		$grid=$this->add("Grid_AccountsBase");
@@ -40,7 +45,7 @@ class page_reports_agent_status extends Page {
 			$acc->addCondition('agent_id',$q->getField('id'));
 			$acc->addCondition('created_at','>=',$date);
 			return $acc->count();
-		})->type('boolean');
+		})->type('boolean')->caption('Active')->sortable(true);
 
 
 		$agent_model->addExpression('sponsor_phone')->set(function($m,$q){
@@ -93,10 +98,16 @@ class page_reports_agent_status extends Page {
 		if($_GET['filter']){
 			$this->api->stickyGET('filter');
 			$this->api->stickyGET('status');
+			$this->api->stickyGET('mo');
 			if($_GET['status']=='Inactive')
 				$agent_model->addCondition('status',0);
 			if($_GET['status']=='Active')
 				$agent_model->addCondition('status','>',0);
+			if($_GET['mo'])
+				$agent_model->addCondition('mo_id',$_GET['mo']);
+			if($_GET['agent'])
+				$agent_model->addCondition('id',$_GET['agent']);
+
 		}else
 			$agent_model->addCondition('id',-1);
 
@@ -104,7 +115,7 @@ class page_reports_agent_status extends Page {
 
 		$agent_model->add('Controller_Acl');
 
-		$grid->setModel($agent_model,array('code','agent_member_name','FatherName','PermanentAddress','PhoneNos','PanNo','account','cadre','current_individual_crpb','last_account_date','sponsor','sponsor_phone','sponsor_cadre','total_accounts','total_amount','created_at','added_by'));
+		$grid->setModel($agent_model,array('code','agent_member_name','status','FatherName','PermanentAddress','PhoneNos','PanNo','account','cadre','current_individual_crpb','last_account_date','sponsor','sponsor_phone','sponsor_cadre','total_accounts','total_amount','created_at','added_by'));
 		$grid->addPaginator(500);
 
 		$grid->addSno();
@@ -113,6 +124,8 @@ class page_reports_agent_status extends Page {
 		$grid->addFormatter('account','wrap');
 
 		$grid->removeColumn('ActiveStatus');
+
+		if($_GET['status']) $grid->removeColumn('status');
 		// $grid->controller->importField('status');
 
 		// $js=array(
@@ -126,7 +139,7 @@ class page_reports_agent_status extends Page {
 		// $grid->js('click',$js);
 
 		if($form->isSubmitted()){
-			$grid->js()->reload(array('status'=>$form['status'],'filter'=>1))->execute();
+			$grid->js()->reload(array('status'=>$form['status'],'mo'=>$form['current_mo_of_agents'],'agent'=>$form['agent'],'filter'=>1))->execute();
 		}
 	}
 }

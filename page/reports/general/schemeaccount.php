a6879bbafeec78c57ab053c7624d35f5c29f8fc4
Gowrav Vishwakarma <gowravvishwakarma@gmail.com>
2018-03-08 18:14:04 +0530

    applied accounts count dig

--- page/reports/general/schemeaccount.php
+++ page/reports/general/schemeaccount.php
@@ -2,19 +2,33 @@
 
 class page_reports_general_schemeaccount extends Page {
 	public $title="Scheme Account Report";
-	function page_index(){
-		// parent::init();
+	
+	function init(){
+		parent::init();
 
-		$from_date = '1970-01-02';
-		$to_date = $this->api->today;
+		$this->account_vp = $this->add('VirtualPage');
+		$this->account_vp->set([$this,'showAccounts']);
 
 		$filter = $this->api->stickyGET('filter');
 		$from_date = $this->api->stickyGET('from_date');
 		$to_date = $this->api->stickyGET('to_date');
 		$selected_scheme = $this->api->stickyGET('scheme');
-		$this->api->stickyGET('account_status');
+		$this->account_status = $this->api->stickyGET('account_status');
 		$status = $this->api->stickyGET('scheme_status');
-		
+
+
+	}
+
+	function page_index(){
+
+		$filter = $this->api->stickyGET('filter');
+		$from_date = $this->api->stickyGET('from_date');
+		$to_date = $this->api->stickyGET('to_date');
+		$selected_scheme = $this->api->stickyGET('scheme');
+		$this->account_status = $this->api->stickyGET('account_status');
+		$status = $this->api->stickyGET('scheme_status');
+
+
 		$scheme_m = $this->add('Model_Scheme');
 
 		// if($_GET['acc_type_scheme']){
@@ -52,7 +66,9 @@ class page_reports_general_schemeaccount extends Page {
 
 		$scheme->addExpression('account_count')->set(function($m,$q){
 			$sum_acc = $m->add('Model_Account')
-					->addCondition('scheme_id',$q->getField('id'));
+					->addCondition('scheme_id',$q->getField('id'))
+					->addCondition('DefaultAC',false)
+					;
 			if($_GET['account_status'] !=='all'){
 				$sum_acc->addCondition('ActiveStatus',$_GET['status']==0?false:true);
 			}
@@ -68,7 +84,9 @@ class page_reports_general_schemeaccount extends Page {
 		});
 		$scheme->addExpression('sum_amount')->set(function($m,$q){
 			$sum_acc = $m->add('Model_Account')
-					->addCondition('scheme_id',$q->getField('id'));
+					->addCondition('scheme_id',$q->getField('id'))
+					->addCondition('DefaultAC',false)
+					;
 			if($_GET['account_status'] !=='all'){
 				$sum_acc->addCondition('ActiveStatus',$_GET['status']==0?false:true);
 			}		
@@ -106,10 +124,14 @@ class page_reports_general_schemeaccount extends Page {
 		}else{
 				$scheme->addCondition('id',-1);
 		}
+		$scheme->addCondition('account_count','>',0);
+
 		// $scheme->add('Controller_Acl');
 		$grid->setModel($scheme,array('name','account_count','sum_amount'));
 		
-		
+		$grid->setFormatter('account_count','template')->setTemplate('<a class="account" href="#" data-scheme_id="{$id}">{$account_count}</a>');
+		$grid->js('click')->_selector('.account')->univ()->frameURL('Accounts Details',[$this->account_vp->getURL(),'scheme_id'=>$this->js()->_selectorThis()->data('scheme_id')]);
+
 		if($form->isSubmitted()){
 			$grid->js()->reload(
 						array(
@@ -123,6 +145,19 @@ class page_reports_general_schemeaccount extends Page {
 							)
 						)->execute();
 		}	
+
+	}
+
+	function showAccounts($page){
+		$grid = $page->add('Grid');
+
+		$accounts = $page->add('Model_Account');
+		$accounts->addCondition('created_at','>=',$_GET['from_date']);
+		$accounts->addCondition('created_at','<',$this->app->nextDate($_GET['to_date']));
+		$accounts->addCondition('scheme_id',$_GET['scheme_id']);
+		$accounts->addCondition('DefaultAC',false);
+
+		$grid->setModel($accounts,['AccountNumber','member_name_only','Amount','created_at','agent']);
 
 	}
 

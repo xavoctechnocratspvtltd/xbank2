746a566819301be026717594ed4bbad774e8ad09
Gowrav Vishwakarma <gowravvishwakarma@gmail.com>
2018-12-06 15:06:07 +0530

    added a few expressiones

--- lib/Model/ShareCertificate.php
+++ lib/Model/ShareCertificate.php
@@ -12,6 +12,21 @@ class Model_ShareCertificate extends Model_Table {
 		$this->addField('created_at')->type('datetime')->defaultValue($this->app->now);
 		$this->addField('submitted_at')->type('datetime')->defaultValue($this->app->now);
 
+		$this->addExpression('start_share')->set(function($m,$q){
+			return $m->add('Model_Share')->addCondition('share_certificate_id',$q->getField('id'))
+					->_dsql()->del('fields')->field($q->expr('MIN(no)'));
+		});
+
+		$this->addExpression('end_share')->set(function($m,$q){
+			return $m->add('Model_Share')->addCondition('share_certificate_id',$q->getField('id'))
+					->_dsql()->del('fields')->field($q->expr('MAX(no)'));
+		});
+
+		$this->addExpression('share_count')->set(function($m,$q){
+			return $m->add('Model_Share')->addCondition('share_certificate_id',$q->getField('id'))
+				->count();
+		});
+
 		$this->hasMany('Share','share_certificate_id');
 	}
 

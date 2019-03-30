427f524ffb9c081480d3305565753de7e31363f3
sinharakesh <rksinha.btech@gmail.com>
2019-03-30 11:22:10 +0530

    loan scheme account change after 1 may

--- lib/Model/Scheme/Loan.php
+++ lib/Model/Scheme/Loan.php
@@ -67,16 +67,28 @@ class Model_Scheme_Loan extends Model_Scheme {
 
 
 	function getDefaultAccounts(){
-		return array(
+		// change account name if scheme is created after 31-march-2019 
+		if(strtotime($this->app->today) > strtotime('2019-03-31')){
+			return array(
 				array('under_scheme'=>"Indirect Income",'intermediate_text'=>"Interest Received On",'Group'=>'Interest Received On {{Loan}}','PAndLGroup'=>'Interest Received On Loan'),
-				array('under_scheme'=>"Indirect Income",'intermediate_text'=>"Processing Fee Received On",'Group'=>'Processing Fee Received On {{Loan}}','PAndLGroup'=>'Processing Fee Received On Loan'),
-				array('under_scheme'=>"Indirect Income",'intermediate_text'=>"Penalty Due To Late Payment On",'Group'=>'Penalty Due To Late Payment On {{Loan}}','PAndLGroup'=>'Penalty Due To Late Payment On Loan'),
-				// array('under_scheme'=>"Indirect Income",'intermediate_text'=>"For Close Account On",'Group'=>'For Close Account On {{Loan}}','PAndLGroup'=>'For Close Account On Loan'),
-				array('under_scheme'=>"Indirect Income",'intermediate_text'=>"Time Over Charge On",'Group'=>'Time Over Charge On {{Loan}}','PAndLGroup'=>'Time Over Charge On Loan'),
-				// array('under_scheme'=>"Indirect Income",'intermediate_text'=>"Conveyence Charge Received On",'Group'=>'Conveyence Charge Received On {{Loan}}','PAndLGroup'=>'Conveyence Charge Received On Loan'),
-				// array('under_scheme'=>"Indirect Income",'intermediate_text'=>"Rent Charge Received On",'Group'=>'Rent Charge Received On {{Loan}}','PAndLGroup'=>'Rent Charge Received On Loan'),
-				// array('under_scheme'=>"Indirect Income",'intermediate_text'=>"Legal Charge Received On",'Group'=>'Legal Charge Received On {{Loan}}','PAndLGroup'=>'Legal Charge Received On Loan'),
+				array('under_scheme'=>"Indirect Income",'intermediate_text'=>"Pre Interest Received On",'Group'=>'Pre Interest Received On {{Loan}}','PAndLGroup'=>'Pre Interest Received On Loan'),
+				array('under_scheme'=>"Indirect Income",'intermediate_text'=>"Penal Interest Received On",'Group'=>'Penal Interest Received On {{Loan}}','PAndLGroup'=>'Penal Interest Received On Loan'),
+				array('under_scheme'=>"Indirect Income",'intermediate_text'=>"Time Over Interest On",'Group'=>'Time Over Interest On {{Loan}}','PAndLGroup'=>'Time Over Interest On Loan'),
 			);
+
+		}else{
+			return array(
+					array('under_scheme'=>"Indirect Income",'intermediate_text'=>"Interest Received On",'Group'=>'Interest Received On {{Loan}}','PAndLGroup'=>'Interest Received On Loan'),
+					array('under_scheme'=>"Indirect Income",'intermediate_text'=>"Processing Fee Received On",'Group'=>'Processing Fee Received On {{Loan}}','PAndLGroup'=>'Processing Fee Received On Loan'),
+					array('under_scheme'=>"Indirect Income",'intermediate_text'=>"Penalty Due To Late Payment On",'Group'=>'Penalty Due To Late Payment On {{Loan}}','PAndLGroup'=>'Penalty Due To Late Payment On Loan'),
+					// array('under_scheme'=>"Indirect Income",'intermediate_text'=>"For Close Account On",'Group'=>'For Close Account On {{Loan}}','PAndLGroup'=>'For Close Account On Loan'),
+					array('under_scheme'=>"Indirect Income",'intermediate_text'=>"Time Over Charge On",'Group'=>'Time Over Charge On {{Loan}}','PAndLGroup'=>'Time Over Charge On Loan'),
+					// array('under_scheme'=>"Indirect Income",'intermediate_text'=>"Conveyence Charge Received On",'Group'=>'Conveyence Charge Received On {{Loan}}','PAndLGroup'=>'Conveyence Charge Received On Loan'),
+					// array('under_scheme'=>"Indirect Income",'intermediate_text'=>"Rent Charge Received On",'Group'=>'Rent Charge Received On {{Loan}}','PAndLGroup'=>'Rent Charge Received On Loan'),
+					// array('under_scheme'=>"Indirect Income",'intermediate_text'=>"Legal Charge Received On",'Group'=>'Legal Charge Received On {{Loan}}','PAndLGroup'=>'Legal Charge Received On Loan'),
+				);			
+		}
+
 	}
 
 	function daily($branch= null,$on_date=null,$test_account=null){

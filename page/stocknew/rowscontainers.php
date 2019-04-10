49ec640c9a24697b099d51ec252cdd9995b38ec9
sinharakesh <rksinha.btech@gmail.com>
2019-04-10 15:29:30 +0530

    row containers current branch only

--- page/stocknew/rowscontainers.php
+++ page/stocknew/rowscontainers.php
@@ -12,8 +12,13 @@ class page_stocknew_rowscontainers extends Page {
 		$container_rows_tab  = $tabs->addTab('Containers & Rows');
 		$container_types_tab = $tabs->addTab('Container Types');
 
+		$model = $this->add('Model_StockNew_Container');
+		$branch_field = $model->getElement('branch_id');
+		$branch_field->getModel()->addCondition('id',$this->app->current_branch->id);
+		$branch_field->defaultValue($this->app->current_branch->id);
+
 		$crud = $container_rows_tab->add('CRUD');
-		$crud->setModel('StockNew_Container');
+		$crud->setModel($model);
 		$crud->add('Controller_Acl',['default_view'=>false]);
 
 		$row_crud = $crud->addRef('StockNew_ContainerRow');

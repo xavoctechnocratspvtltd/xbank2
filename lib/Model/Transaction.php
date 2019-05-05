7095303290973de0c47b39bc44c9e4608582327e
sinharakesh <rksinha.btech@gmail.com>
2019-05-05 14:39:54 +0530

    inovice number start from 1

--- lib/Model/Transaction.php
+++ lib/Model/Transaction.php
@@ -453,9 +453,9 @@ class Model_Transaction extends Model_Table {
 		$transaction_model = $this->add('Model_Transaction');
 		$transaction_model->addCondition('created_at','>=',$start_date);
 		$transaction_model->addCondition('created_at','<',$this->api->nextDate($end_date)); // ! important next date
-		$max_inv_no = $transaction_model->_dsql()->del('fields')->field('max(CAST(invoice_no AS int))')->getOne();
-		if($max_inv_no > 766) return $max_inv_no+1;
-		return 767;
+		return $transaction_model->_dsql()->del('fields')->field('max(CAST(invoice_no AS int))')->getOne() + 1;
+		// if($max_inv_no > 766) return $max_inv_no+1;
+		// return 767;
 	}
 
 	// function __destruct(){

1d19b742124e30c8949fa220c18ce1995d02fc65
sinharakesh <rksinha.btech@gmail.com>
2019-06-06 23:07:55 +0530

    few more expression added and some columns are removed

--- page/reports/loan/bikelegal/bikescasedetailreport.php
+++ page/reports/loan/bikelegal/bikescasedetailreport.php
@@ -52,81 +52,86 @@ class page_reports_loan_bikelegal_bikescasedetailreport extends Page {
 		$account_model->addExpression('emi_amount')->set(function($m,$q){
 			return $m->refSQL('Premium')->setLImit(1)->fieldQuery('Amount');
 		});
-
-		$account_model->addExpression('due_premium_count')->set(function($m,$q){
-			$p_m = $m->refSQL('Premium')
-						->addCondition('PaidOn',null);
-			$p_m->addCondition('DueDate','<',$m->api->nextDate($this->app->now));
-			return $p_m->count();
+		$account_model->addExpression('maturity_date')->set(function($m,$q){
+			return $m->refSQL('Premium')->setOrder('id','desc')->setLImit(1)->fieldQuery('DueDate');
 		});
 
-		$account_model->addExpression('paid_premium_count')->set(function($m,$q){
-			$p_m=$m->refSQL('Premium')
-						->addCondition('PaidOn','<>',null);
-			$p_m->addCondition('DueDate','<',$m->api->nextDate($this->app->today));
-			return $p_m->count();
-		})->sortable(true);
+		// $account_model->addExpression('due_premium_count')->set(function($m,$q){
+		// 	$p_m = $m->refSQL('Premium')
+		// 				->addCondition('PaidOn',null);
+		// 	$p_m->addCondition('DueDate','<',$m->api->nextDate($this->app->now));
+		// 	return $p_m->count();
+		// });
+
+		// $account_model->addExpression('paid_premium_count')->set(function($m,$q){
+		// 	$p_m=$m->refSQL('Premium')
+		// 				->addCondition('PaidOn','<>',null);
+		// 	$p_m->addCondition('DueDate','<',$m->api->nextDate($this->app->today));
+		// 	return $p_m->count();
+		// })->sortable(true);
 
 		$account_model->addExpression('due_premium_amount')->set(function($m,$q){
 			return $q->expr('[0]*[1]',[$m->getElement('due_premium_count'),$m->getElement('emi_amount')]);
 		});
 
-		$account_model->addExpression('due_panelty')->set(function($m,$q){
-			$trans_type = $this->add('Model_TransactionType')->tryLoadBy('name',TRA_PENALTY_ACCOUNT_AMOUNT_DEPOSIT);
+		// $account_model->addExpression('due_panelty')->set(function($m,$q){
+		// 	$trans_type = $this->add('Model_TransactionType')->tryLoadBy('name',TRA_PENALTY_ACCOUNT_AMOUNT_DEPOSIT);
 			
-			$tr_m_due = $m->add('Model_TransactionRow',array('table_alias'=>'charged_panelty_tr'));
-			$tr_m_due->addCondition('transaction_type_id',$trans_type->id); 
-			$tr_m_due->addCondition('account_id',$q->getField('id'));
-			$tr_m_due->addCondition('created_at','<',$this->app->nextDate($this->app->today));
+		// 	$tr_m_due = $m->add('Model_TransactionRow',array('table_alias'=>'charged_panelty_tr'));
+		// 	$tr_m_due->addCondition('transaction_type_id',$trans_type->id); 
+		// 	$tr_m_due->addCondition('account_id',$q->getField('id'));
+		// 	$tr_m_due->addCondition('created_at','<',$this->app->nextDate($this->app->today));
 
-			$trans_type = $this->add('Model_TransactionType')->tryLoadBy('name',TRA_PENALTY_AMOUNT_RECEIVED);
+		// 	$trans_type = $this->add('Model_TransactionType')->tryLoadBy('name',TRA_PENALTY_AMOUNT_RECEIVED);
 			
-			$tr_m_received = $m->add('Model_TransactionRow',array('table_alias'=>'received_panelty_tr'));
-			$tr_m_received->addCondition('transaction_type_id',$trans_type->id); 
-			$tr_m_received->addCondition('account_id',$q->getField('id'));
-			$tr_m_received->addCondition('created_at','<',$this->app->nextDate($this->app->today));
+		// 	$tr_m_received = $m->add('Model_TransactionRow',array('table_alias'=>'received_panelty_tr'));
+		// 	$tr_m_received->addCondition('transaction_type_id',$trans_type->id); 
+		// 	$tr_m_received->addCondition('account_id',$q->getField('id'));
+		// 	$tr_m_received->addCondition('created_at','<',$this->app->nextDate($this->app->today));
 
-			return $q->expr('(IFNULL([0],0)-IFNULL([1],0))',[$tr_m_due->sum('amountDr'),$tr_m_received->sum('amountCr')]);
-		});
+		// 	return $q->expr('(IFNULL([0],0)-IFNULL([1],0))',[$tr_m_due->sum('amountDr'),$tr_m_received->sum('amountCr')]);
+		// });
 
-		$account_model->addExpression('total_cr')->set(function($m,$q){
-			$tr_m = $m->add('Model_TransactionRow',array('table_alias'=>'other_charges_tr'));
-			$tr_m->addCondition('account_id',$q->getField('id'));
-			return $received = $tr_m->sum('amountCr');
-		});
+		// $account_model->addExpression('total_cr')->set(function($m,$q){
+		// 	$tr_m = $m->add('Model_TransactionRow',array('table_alias'=>'other_charges_tr'));
+		// 	$tr_m->addCondition('account_id',$q->getField('id'));
+		// 	return $received = $tr_m->sum('amountCr');
+		// });
 
-		$account_model->addExpression('other_charges')->set(function($m,$q){
-			$tr_m = $m->add('Model_TransactionRow',array('table_alias'=>'other_charges_tr'));
-			$tr_m->addCondition('transaction_type_id',[13, 46, 39]); // JV, TRA_VISIT_CHARGE, LegalChargeReceived
-			$tr_m->addCondition('account_id',$q->getField('id'));
-			return $tr_m->sum('amountDr');
-		});
+		// $account_model->addExpression('other_charges')->set(function($m,$q){
+		// 	$tr_m = $m->add('Model_TransactionRow',array('table_alias'=>'other_charges_tr'));
+		// 	$tr_m->addCondition('transaction_type_id',[13, 46, 39]); // JV, TRA_VISIT_CHARGE, LegalChargeReceived
+		// 	$tr_m->addCondition('account_id',$q->getField('id'));
+		// 	return $tr_m->sum('amountDr');
+		// });
 
-		$account_model->addExpression('premium_amount_received')->set(function($m,$q){
-			return $premium_paid = $q->expr('([0]*[1])',[$m->getElement('paid_premium_count'),$m->getElement('emi_amount')]);
-		});
+		// $account_model->addExpression('premium_amount_received')->set(function($m,$q){
+		// 	return $premium_paid = $q->expr('([0]*[1])',[$m->getElement('paid_premium_count'),$m->getElement('emi_amount')]);
+		// });
 
-		$account_model->addExpression('penalty_amount_received')->set(function($m,$q){
-			$trans_type = $this->add('Model_TransactionType')->tryLoadBy('name',TRA_PENALTY_AMOUNT_RECEIVED);
+		// $account_model->addExpression('penalty_amount_received')->set(function($m,$q){
+		// 	$trans_type = $this->add('Model_TransactionType')->tryLoadBy('name',TRA_PENALTY_AMOUNT_RECEIVED);
 			
-			$tr_m_received = $m->add('Model_TransactionRow',array('table_alias'=>'other_received_panelty_tr'));
-			$tr_m_received->addCondition('transaction_type_id',$trans_type->id); 
-			$tr_m_received->addCondition('account_id',$q->getField('id'));
-			$tr_m_received->addCondition('created_at','<',$this->app->nextDate($this->app->today));
-			return $tr_m_received->sum('amountCr');
-		});
+		// 	$tr_m_received = $m->add('Model_TransactionRow',array('table_alias'=>'other_received_panelty_tr'));
+		// 	$tr_m_received->addCondition('transaction_type_id',$trans_type->id); 
+		// 	$tr_m_received->addCondition('account_id',$q->getField('id'));
+		// 	$tr_m_received->addCondition('created_at','<',$this->app->nextDate($this->app->today));
+		// 	return $tr_m_received->sum('amountCr');
+		// });
 
-		$account_model->addExpression('other_received')->set(function($m,$q){
-			return $q->expr('(IFNULL([0],0)-(IFNULL([1],0)+IFNULL([2],0)))',[$m->getElement('total_cr'),$m->getElement('premium_amount_received'),$m->getElement('penalty_amount_received')]);
-		});
+		// $account_model->addExpression('other_received')->set(function($m,$q){
+		// 	return $q->expr('(IFNULL([0],0)-(IFNULL([1],0)+IFNULL([2],0)))',[$m->getElement('total_cr'),$m->getElement('premium_amount_received'),$m->getElement('penalty_amount_received')]);
+		// });
 
-		$account_model->addExpression('other_charges_due')->set(function($m,$q){
-			return $q->expr('(IFNULL([0],0)-IFNULL([1],0))',[$m->getElement('other_charges'),$m->getElement('other_received')]);
-		});
+		// $account_model->addExpression('other_charges_due')->set(function($m,$q){
+		// 	return $q->expr('(IFNULL([0],0)-IFNULL([1],0))',[$m->getElement('other_charges'),$m->getElement('other_received')]);
+		// });
 
-		$account_model->addExpression('total_due')->set(function($m,$q){
-			return $q->expr('(IFNULL([0],0)+IFNULL([1],0)+IFNULL([2],0))',[$m->getElement('due_premium_amount'),$m->getElement('due_panelty'),$m->getElement('other_charges_due')]);
-		});
+		// $account_model->addExpression('total_due')->set(function($m,$q){
+		// 	return $q->expr('(IFNULL([0],0)+IFNULL([1],0)+IFNULL([2],0))',[$m->getElement('due_premium_amount'),$m->getElement('due_panelty'),$m->getElement('other_charges_due')]);
+		// });
+
+		$account_model->addExpression('current_balance')->set('(CurrentBalanceDr-CurrentBalanceCr)');
 
 		$account_model->addExpression('last_hearing_stage')->set(function($m,$q){
 			return $this->add('Model_LegalCaseHearing')
@@ -135,7 +140,8 @@ class page_reports_loan_bikelegal_bikescasedetailreport extends Page {
 					->fieldQuery('stage');
 		});
 
-		$grid_column_array = ['AccountNumber','member','FatherName','PermanentAddress','landmark','tehsil','district','PhoneNos','dealer','member_sm_account','bike_surrendered_on','Amount','no_of_emi','emi_amount','due_premium_amount','due_panelty','other_charges','total_cr','premium_amount_received','penalty_amount_received','other_received','other_charges_due','total_due','created_at','ActiveStatus','last_hearing_stage'];
+		$grid_column_array = ['AccountNumber','member','FatherName','PermanentAddress','landmark','tehsil','district','PhoneNos','dealer','member_sm_account','bike_surrendered_on','Amount','no_of_emi','emi_amount','premium_amount_received','current_balance','created_at','ActiveStatus','maturity_date','last_hearing_stage'];
+		// $grid_column_array = ['AccountNumber','member','FatherName','PermanentAddress','landmark','tehsil','district','PhoneNos','dealer','member_sm_account','bike_surrendered_on','Amount','no_of_emi','emi_amount','due_premium_amount','due_panelty','other_charges','total_cr','premium_amount_received','penalty_amount_received','other_received','other_charges_due','total_due','created_at','ActiveStatus','last_hearing_stage'];
 
 		$grid = $this->add('Grid_AccountsBase')->addSno();
 		if($this->api->stickyGET('filter')){
